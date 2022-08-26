8370e4b5c59978b24d11f487aab7e36139a2ee7c
ralphZero <placideralph@gmail.com>
2022-08-26 01:35:48 -0400

    frontend can talk with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+interface Greeting {
+  message: string;
+}
 
 function App() {
+  const [greeting, setGreeting] = useState<string>('');
+
+  useEffect(() => {
+    fetch('https://studyhall-io-api.web.app/')
+    .then((res: Response): Promise<Greeting> => res.json())
+    .then((data) => setGreeting(data.message))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { greeting }
     </div>
   );
 }
