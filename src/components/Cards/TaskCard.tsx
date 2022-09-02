53040635cef037e9f42d685eba45a9066d895b82
ralphZero <placideralph@gmail.com>
2022-09-02 12:59:08 -0400

    before authentication

--- src/components/Cards/TaskCard.tsx
+++ src/components/Cards/TaskCard.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import { Card, Space } from 'antd';
-import { Task } from '../../models/task';
-import { CheckCircleFilled } from '@ant-design/icons';
+import React from "react";
+import { Card, Progress, Space } from "antd";
+import { Task } from "../../models/task";
+import { CheckCircleFilled, CheckCircleOutlined } from "@ant-design/icons";
 
 interface TaskCardProps {
-  task: Task,
-  onClick: (e: any, task: Task) => void
+  task: Task;
+  onClick: (e: any, task: Task) => void;
 }
 
 const TaskCard = ({ task, onClick }: TaskCardProps) => {
-  const completeIcon = task.isComplete ? { color: "#6F9600", display: "block" } : { color: "#6F9600", display: "none" }
+  const completeIcon = task.isComplete
+    ? { color: "#6F9600", display: "block" }
+    : { color: "#6F9600", display: "none" };
   return (
-    <Card onClick={(e) => onClick(e, task)} extra={<CheckCircleFilled style={completeIcon} />} hoverable size="small" title={task.label} style={{ width: "100%", margin: "12px 0px" }}>
-        <div className='task-card-title'>
-          <Space wrap>
-            {task.task}
+    <Card
+      onClick={(e) => onClick(e, task)}
+      extra={<CheckCircleFilled style={completeIcon} />}
+      hoverable
+      size="small"
+      title={task.label}
+      style={{ width: "100%", margin: "12px 0px" }}
+    >
+      <div className="task-card-title">
+        <Space wrap>{task.task}</Space>
+      </div>
+      {task.subtasksCount > 0 && (
+        <Space
+          style={{ display: "flex", paddingBlock: 15 }}
+          direction="vertical"
+        >
+          <Progress percent={Math.round(task.progress * 100)} />
+          <Space>
+            <CheckCircleOutlined />
+            {`${task.subtasksCompletedCount} / ${task.subtasksCount}`} completed
           </Space>
-        </div>
-        {/* <Progress percent={Math.round(progress * 100)} /> */}
+        </Space>
+      )}
     </Card>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
---
+++ src/context/UserContext.tsx
@@ -0,0 +1,6 @@
+import React, { createContext } from 'react';
+
+interface UserContextType {
+
+}
+const UserContext = createContext<UserContextType>();
