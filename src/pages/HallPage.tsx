e21aad43d6db1375b4f8a16f499105121a6b4753
ralphZero <placideralph@gmail.com>
2023-01-14 21:39:51 -0500

    wip: add filter to <App /> - quick code format

--- src/pages/HallPage.tsx
+++ src/pages/HallPage.tsx
@@ -1,46 +1,41 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { DragDropContext, DropResult } from 'react-beautiful-dnd'
-import HallTitle from '../components/Headers/HallTitle'
-import Header from '../components/Headers/Header'
-import KanbanContainer from '../components/Containers/KanbanContainer'
-import KanbanCol from '../components/Containers/KanbanCol'
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import HallTitle from '../components/Headers/HallTitle';
+import Header from '../components/Headers/Header';
+import KanbanContainer from '../components/Containers/KanbanContainer';
+import KanbanCol from '../components/Containers/KanbanCol';
 
-import CreateTaskModal, { Values } from '../components/Modals/CreateTaskModal'
-import { Task } from '../models/task'
-import { Hall } from '../models/hall'
-import { DataContext } from '../context/DataContext'
-import TaskDrawer from '../components/Drawers/TaskDrawer'
-import { PlanDate } from '../models/plandate'
-import HallPageSkeleton from '../components/skeletons/HallPageSkeleton'
+import CreateTaskModal, { Values } from '../components/Modals/CreateTaskModal';
+import { Task } from '../models/task';
+import { Hall } from '../models/hall';
+import { DataContext } from '../context/DataContext';
+import TaskDrawer from '../components/Drawers/TaskDrawer';
+import { PlanDate } from '../models/plandate';
+import HallPageSkeleton from '../components/skeletons/HallPageSkeleton';
 
 const HallPage = () => {
-  const [visibleModal, setVisibleModal] = useState<boolean>(false)
-  const [visibleDrawer, setVisibleDrawer] = useState<boolean>(false)
+  const [visibleModal, setVisibleModal] = useState<boolean>(false);
+  const [visibleDrawer, setVisibleDrawer] = useState<boolean>(false);
 
-  const [selectedCol, setSelectedCol] = useState<string>('')
-  const [selectedTask, setSelectedTask] = useState<Task>()
+  const [selectedCol, setSelectedCol] = useState<string>('');
+  const [selectedTask, setSelectedTask] = useState<Task>();
 
-  const [hall, setHall] = useState<Hall>()
+  const [hall, setHall] = useState<Hall>();
 
-  const { hallId } = useParams()
+  const { hallId } = useParams();
 
   const {
-    dataList,
+    prepareCurrentHall,
     createTaskInHall,
     updateTaskInHall,
     isLoading,
     updateDatesInHall,
-  } = useContext(DataContext)
-
-  useEffect(() => {
-    const thisHall = dataList.filter((data) => data._id === hallId)[0]
-    setHall(thisHall)
-  }, [hallId, dataList])
+  } = useContext(DataContext);
 
   const onCreateTask = (values: Values) => {
-    const selectedDateId = selectedCol
+    const selectedDateId = selectedCol;
     // Create Task to send to API
     const task: Task = {
       dateId: selectedDateId,
@@ -52,34 +47,34 @@ const HallPage = () => {
       subtasksCompletedCount: 0,
       progress: 0,
       priority: 0,
-    }
+    };
     createTaskInHall(hallId as string, task, () => {
-      setVisibleModal(false)
-    })
-    setSelectedCol('')
-  }
+      setVisibleModal(false);
+    });
+    setSelectedCol('');
+  };
 
   const onUpdateTask = (values: Task) => {
-    updateTaskInHall(hallId as string, values)
-    setSelectedTask(undefined)
-    setVisibleDrawer(false)
-  }
+    updateTaskInHall(hallId as string, values);
+    setSelectedTask(undefined);
+    setVisibleDrawer(false);
+  };
 
   const handleCardClick = (e: any, task: Task) => {
-    setSelectedTask(task)
-    setVisibleDrawer(true)
-  }
+    setSelectedTask(task);
+    setVisibleDrawer(true);
+  };
 
   const onCloseDrawerTask = () => {
-    setVisibleDrawer(false)
-    setSelectedTask(undefined)
-  }
+    setVisibleDrawer(false);
+    setSelectedTask(undefined);
+  };
 
   const onDragEnd = (result: DropResult) => {
-    const { destination, source, draggableId } = result
+    const { destination, source, draggableId } = result;
 
     if (!destination) {
-      return
+      return;
     }
 
     // if dragged back to where it was
@@ -87,49 +82,54 @@ const HallPage = () => {
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
-      return
+      return;
     }
 
     if (hall) {
       const start = hall.dates.find(
         (col) => col.id === source.droppableId
-      ) as PlanDate
+      ) as PlanDate;
       const finish = hall.dates.find(
         (col) => col.id === destination.droppableId
-      ) as PlanDate
+      ) as PlanDate;
 
       if (start === finish) {
-        const newTaskIds = Array.from(start.taskIds)
-        newTaskIds.splice(source.index, 1)
-        newTaskIds.splice(destination.index, 0, draggableId)
+        const newTaskIds = Array.from(start.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
 
-        const colIndex = hall.dates.findIndex((col) => col.id === start.id)
-        const updatedTaskIds = (hall.dates[colIndex].taskIds = newTaskIds)
-        const dates = [...hall.dates, updatedTaskIds] as PlanDate[]
+        const colIndex = hall.dates.findIndex((col) => col.id === start.id);
+        const updatedTaskIds = (hall.dates[colIndex].taskIds = newTaskIds);
+        const dates = [...hall.dates, updatedTaskIds] as PlanDate[];
 
-        updateDatesInHall(hallId as string, dates)
+        updateDatesInHall(hallId as string, dates);
 
-        return
+        return;
       }
 
-      const startTaskIds = Array.from(start.taskIds)
-      startTaskIds.splice(source.index, 1)
+      const startTaskIds = Array.from(start.taskIds);
+      startTaskIds.splice(source.index, 1);
 
-      const finishTaskIds = Array.from(finish.taskIds)
-      finishTaskIds.splice(destination.index, 0, draggableId)
+      const finishTaskIds = Array.from(finish.taskIds);
+      finishTaskIds.splice(destination.index, 0, draggableId);
 
-      const startIndex = hall.dates.findIndex((col) => col.id === start.id)
-      const endIndex = hall.dates.findIndex((col) => col.id === finish.id)
+      const startIndex = hall.dates.findIndex((col) => col.id === start.id);
+      const endIndex = hall.dates.findIndex((col) => col.id === finish.id);
 
       const dates = [
         ...hall.dates,
         (hall.dates[startIndex].taskIds = startTaskIds),
         (hall.dates[endIndex].taskIds = finishTaskIds),
-      ] as PlanDate[]
+      ] as PlanDate[];
 
-      updateDatesInHall(hallId as string, dates)
+      updateDatesInHall(hallId as string, dates);
     }
-  }
+  };
+
+  useEffect(() => {
+    const thisHall = prepareCurrentHall(hallId as string);
+    setHall(thisHall);
+  }, [hallId, prepareCurrentHall]);
 
   return hall ? (
     <>
@@ -144,11 +144,21 @@ const HallPage = () => {
           {hall.dateIds.map((dateId) => {
             const dateColumn = hall.dates.find(
               (column) => column.id === dateId
-            ) as PlanDate
+            ) as PlanDate;
+
+            // const firstDate = moment(currentWeek[0]);
+
+            // const lastDate = moment(currentWeek[currentWeek.length - 1]);
+
+            // const thisDate = moment(dateColumn.date);
+
+            // if (!thisDate.isBetween(firstDate, lastDate, 'week', '[]')) {
+            //   return null;
+            // }
 
             const tasks = dateColumn.taskIds.map((taskId) =>
               hall.tasks.find((task) => task.id === taskId)
-            ) as Task[]
+            ) as Task[];
             return (
               <KanbanCol
                 handleCardClick={handleCardClick}
@@ -158,7 +168,7 @@ const HallPage = () => {
                 onToggleModal={setVisibleModal}
                 key={dateColumn.id}
               />
-            )
+            );
           })}
         </DragDropContext>
       </KanbanContainer>
@@ -178,7 +188,7 @@ const HallPage = () => {
     </>
   ) : (
     <HallPageSkeleton title='loading' />
-  )
-}
+  );
+};
 
-export default HallPage
+export default HallPage;
