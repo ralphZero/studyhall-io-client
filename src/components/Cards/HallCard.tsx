d6aad466045980557957a4733f57f2fd34078cf8
ralphZero <placideralph@gmail.com>
2023-05-09 20:46:56 -0400

    update hallcard link

--- src/components/Cards/HallCard.tsx
+++ src/components/Cards/HallCard.tsx
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
-import { Button, Card, Popconfirm, Progress, Space } from "antd";
-import { Link } from "react-router-dom";
-import moment from "moment";
-import { DeleteOutlined } from "@ant-design/icons";
-import { DataContext } from "../../context/DataContext";
+import React, { useContext } from 'react';
+import { Button, Card, Popconfirm, Progress, Space } from 'antd';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import { DeleteOutlined } from '@ant-design/icons';
+import { DataContext } from '../../context/DataContext';
 
 interface HallCardProps {
   id: string;
@@ -30,20 +30,19 @@ const HallCard = ({
   };
 
   const extras = (
-    <Space size="middle">
-      <Button className="kanban-col-header-btn" type="text">
-        <Link to={`/halls/${id}`}>open</Link>
+    <Space size='middle'>
+      <Button className='kanban-col-header-btn' type='text'>
+        <Link to={`/app/halls/${id}`}>open</Link>
       </Button>
       <Popconfirm
-      style={{ padding: "3px" }}
-        title="Are you sure to delete this task?"
+        style={{ padding: '3px' }}
+        title='Are you sure to delete this task?'
         onConfirm={onDeleteHall}
-        okText="Yes"
-        cancelText="No"
-      >
+        okText='Yes'
+        cancelText='No'>
         <Button
-          className="kanban-col-header-btn"
-          type="text"
+          className='kanban-col-header-btn'
+          type='text'
           icon={<DeleteOutlined />}
         />
       </Popconfirm>
@@ -54,20 +53,19 @@ const HallCard = ({
   const formattedEnd = moment(`${endDate}T12:00:00.000Z`);
 
   return (
-    <Card extra={extras} size="small" style={{ width: "100%" }}>
-      <p className="hall-card-title">{title}</p>
-      <p className="hall-card-description">{description}</p>
+    <Card extra={extras} size='small' style={{ width: '100%' }}>
+      <p className='hall-card-title'>{title}</p>
+      <p className='hall-card-description'>{description}</p>
       <Progress percent={Math.round(progress * 100)} />
       <Space
         style={{
-          display: "flex",
-          justifyContent: "space-between",
+          display: 'flex',
+          justifyContent: 'space-between',
           paddingBlock: 8,
-          color: "GrayText",
-        }}
-      >
-        <span>{moment(formattedStart).format("ll")}</span>
-        <span>{moment(formattedEnd).format("ll")}</span>
+          color: 'GrayText',
+        }}>
+        <span>{moment(formattedStart).format('ll')}</span>
+        <span>{moment(formattedEnd).format('ll')}</span>
       </Space>
     </Card>
   );
