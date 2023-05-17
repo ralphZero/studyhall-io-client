4f41bbb4b8aefc289c4453e95687ea413c15a744
ralphZero <placideralph@gmail.com>
2023-05-16 22:34:25 -0400

    added root provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import UserContextProvider from "./context/UserContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import UserContextProvider from './context/UserContext';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
+
 root.render(
+  <Provider store={store}>
     <UserContextProvider>
       <App />
     </UserContextProvider>
+  </Provider>
 );
