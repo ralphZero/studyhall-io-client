52f6df9139f422fd5e32f803c898c425e1ef02a7
ralphZero <placideralph@gmail.com>
2023-05-20 19:08:40 -0400

    separate enpoint definitions

--- src/features/api/hallifyApi.ts
+++ src/features/api/hallifyApi.ts
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../../store/store';
+import endpoints from '../../services/endpoints';
 
 export const hallifyApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.hallify.com/',
+    baseUrl: endpoints.baseUrl,
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
       if (token) {
---
+++ src/services/endpoints/index.ts
@@ -0,0 +1,5 @@
+const endpoints = {
+  baseUrl: 'https://api.hallify.com/',
+};
+
+export default endpoints;
