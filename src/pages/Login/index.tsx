8daeb99163551e429412ba25662dbb1d8a8ba93b
ralphZero <placideralph@gmail.com>
2023-05-20 19:42:12 -0400

    build login page scaffold content

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,17 @@
+import { Button, Layout } from 'antd';
 import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Layout className='h-screen'>
+      <Layout.Content className='flex flex-col justify-center items-center'>
+        <h1>Login</h1>
+        <Button type='primary' size='large' className='w-36'>
+          Sign in
+        </Button>
+      </Layout.Content>
+    </Layout>
+  );
 };
 
 export default Login;
