c1d7e7b2cceb361c298ba4d00912aaf98ed5cba2
ralphZero <placideralph@gmail.com>
2023-05-20 17:44:18 -0400

    add authentication to baseQuery

--- src/features/api/hallifyApi.ts
+++ src/features/api/hallifyApi.ts
@@ -1,7 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../../store/store';
 
 export const hallifyApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.hallify.com/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.hallify.com/',
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set('authorization', token);
+      }
+    },
+  }),
   endpoints: () => ({}),
 });
