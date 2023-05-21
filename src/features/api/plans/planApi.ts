fc17386b0e051a670e6fac3c834d1c82fddd0304
ralphZero <placideralph@gmail.com>
2023-05-20 20:54:07 -0400

    add new query to get plan by id

--- src/features/api/plans/planApi.ts
+++ src/features/api/plans/planApi.ts
@@ -2,5 +2,11 @@ import { hallifyApi } from '../hallifyApi';
 
 export const planApi = hallifyApi.injectEndpoints({
   overrideExisting: false,
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getPlanById: builder.query({
+      query: (planId) => ({ url: `halls/${planId}` }),
+    }),
+  }),
 });
+
+export const { useGetPlanByIdQuery } = planApi;
