70e84bd360f80a3c609984f0f39d5fe0b47e3ebb
ralphZero <placideralph@gmail.com>
2023-05-23 23:31:26 -0400

    add production env util

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -2,3 +2,8 @@ export const isDevelopment = () => {
   const env = process.env.NODE_ENV;
   return env && env === 'development';
 };
+
+export const isproduction = () => {
+  const env = process.env.NODE_ENV;
+  return env && env === 'production';
+};
